57241d1068621d695ed24ef644b2e4d33fac61fc
unni <aneeshcn01@gmail.com>
2020-06-10 22:51:59 +0200

    Updates

---
+++ precache-manifest.a504f8f4550dd0770c2e54aa25a1569e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5567e1073138c946717bbe9750c5803",
+    "url": "/mrbp/index.html"
+  },
+  {
+    "revision": "bb87cf55309ce839364f",
+    "url": "/mrbp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aae32d1579207a5d2788",
+    "url": "/mrbp/static/js/2.491ad92f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrbp/static/js/2.491ad92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb87cf55309ce839364f",
+    "url": "/mrbp/static/js/main.82aefda2.chunk.js"
+  },
+  {
+    "revision": "046d91ff1e27bbcc77cf",
+    "url": "/mrbp/static/js/runtime-main.0caafbbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mrbp/static/media/logo.5d5d9eef.svg"
+  }
+]);
